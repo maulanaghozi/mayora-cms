506f5f3fa0ea699ff5784267308913e84423859b
maulanaghozi <maulana.ghozi@qbit.co.id>
2021-04-23 12:45:49 +0700

    remove some components

--- src/components/GroupTable/Column/Description.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { table_cell, column_description } from '../GroupTable.module.scss'
-
-export default function Description(props) {
-    return (
-        <div className={table_cell + ' ' + column_description}>{props.row.description}</div>
-    )
-}
