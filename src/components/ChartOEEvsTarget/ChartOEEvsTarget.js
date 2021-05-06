e573ba3c5d7577115cb33ab762f18af4e8ab7266
maulanaghozi <maulana.ghozi@qbit.co.id>
2021-05-06 20:07:16 +0700

    oee chart

---
+++ src/components/ChartOEEvsTarget/ChartOEEvsTarget.js
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+
+class ChartOEEvsTarget extends Component {
+  constructor(props) {
+    console.log(props);
+    super(props);
+
+    this.updateCharts = this.updateCharts.bind(this);
+
+    this.state = {
+      optionsMixedChart: {
+        chart: {
+          id: "basic-bar",
+          toolbar: {
+            show: false,
+          },
+        },
+        plotOptions: {
+          bar: {
+            columnWidth: "50%",
+          },
+        },
+        stroke: {
+          width: [4, 0, 0],
+        },
+        xaxis: {
+          categories: data.categories,
+        },
+        markers: {
+          size: 6,
+          strokeWidth: 3,
+          fillOpacity: 0,
+          strokeOpacity: 0,
+          hover: {
+            size: 8,
+          },
+        },
+        yaxis: {
+          tickAmount: 5,
+          min: 0,
+          max: 1,
+        },
+      },
+      seriesMixedChart: [
+        {
+          name: "Target",
+          type: "line",
+          chart: {
+            background: "#fff",
+          },
+          data: props.dataTarget,
+        },
+        {
+          name: "Actual",
+          type: "column",
+          data: data.actual,
+        },
+      ],
+    };
+  }
+
+  updateCharts() {
+    const max = 90;
+    const min = 30;
+    const newMixedSeries = [];
+    const newBarSeries = [];
+
+    this.state.seriesMixedChart.forEach(s => {
+      const data = s.data.map(() => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      });
+      newMixedSeries.push({ data: data, type: s.type });
+    });
+
+    this.state.seriesBar.forEach(s => {
+      const data = s.data.map(() => {
+        return Math.floor(Math.random() * (180 - min + 1)) + min;
+      });
+      newBarSeries.push({ data, name: s.name });
+    });
+
+    this.setState({
+      seriesMixedChart: newMixedSeries,
+      seriesBar: newBarSeries,
+      seriesRadial: [Math.floor(Math.random() * (90 - 50 + 1)) + 50],
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ width: "100%" }}>
+        <div className="row" style={{ width: "100%" }}>
+          <div className="col mixed-chart">
+            <Chart
+              options={this.state.optionsMixedChart}
+              series={this.state.seriesMixedChart}
+              type="line"
+              width="100%"
+              height="320"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ChartOEEvsTarget;
+
+const data = {
+  target: [0.9, 0.9],
+  actual: [0, 0],
+  categories: ["07", "08"],
+};
