b1d807fea517e838535fbfafc545ff998a2b0f59
maulanaghozi <maulana.ghozi@qbit.co.id>
2021-05-11 13:36:37 +0700

    fixed table oee vs target

--- src/page/Dashboard/Screen/OEEvsTarget/OEEvsTarget.js
+++ src/page/Dashboard/Screen/OEEvsTarget/OEEvsTarget.js
@@ -50,14 +50,14 @@ export default function OEEvsTarget() {
 
     data.forEach(item => {
       const date = moment(item.Date).format("DD MMM");
-      const oee = item.OEE / 100;
+      const oee = Number(item.OEE / 100).toFixed(3);
 
       dates.push(date);
       oeeData.push(oee);
       if (machine === 1) {
-        target.push(targetOEE1 / 100);
+        target.push(Number(targetOEE1 / 100).toFixed(2));
       } else {
-        target.push(targetOEE2 / 100);
+        target.push(Number(targetOEE2 / 100).toFixed(2));
       }
     });
 
