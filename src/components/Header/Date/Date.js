c795cf460fd389cc8c115d1a58d663212bfe3390
maulanaghozi <maulana.ghozi@qbit.co.id>
2021-05-27 07:11:48 +0700

    live date

--- src/components/Header/Date/Date.js
+++ src/components/Header/Date/Date.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { CalendarIcon } from "../../../assets/icons/index";
+import Clock from "react-live-clock";
 import moment from "moment";
 import Styles from "./Date.module.scss";
 
@@ -7,7 +8,13 @@ export default function HelpNotif() {
   return (
     <div className={Styles.container}>
       <CalendarIcon className={Styles.icon} />
-      <span>{moment().format("dddd, DD/MM/YYYY")}</span>
+      <Clock
+        className={Styles.clock}
+        format={"dddd, DD/MM/YYYY"}
+        ticking={true}
+        timezone={"Asia/Jakarta"}
+      />
+      {/* <span>{moment().format("dddd, DD/MM/YYYY")}</span> */}
     </div>
   );
 }
